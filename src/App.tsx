7e403a334ca377f6bc836378b466d6f43e7e4f04
karolinfrennert <karolinfrennert@gmail.com>
2021-01-28 22:01:57 +0100

    First type :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Head} from './components/Head'
 
 function App() {
   return (
     <div className="App">
+      <Head title ="Hello" isActive/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
